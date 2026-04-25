91e7b03f204131772abcdc4e2b22a8b39cfe87d5
prarthanaa69-prog <prarthanaa69@gmail.com>
2026-04-25 15:05:00 +0530

    Update Q01.py

--- Q01.py
+++ Q01.py
@@ -14,3 +14,16 @@
 # Sample Output:  Grade: B
 
 # --- YOUR CODE HERE ---
+score=int(input("Enter your score: "))
+if  90<=score<=100 :
+    print("Grade : A")
+elif  80<=score<=89 :
+    print("Grade : B")
+elif  70<=score<=79 :
+    print("Grade : C")
+elif  60<=score<=69 :
+    print("Grade : D")
+elif  0<=score<60 :
+    print("Grade : F")
+else :
+    print("Invalid input")
