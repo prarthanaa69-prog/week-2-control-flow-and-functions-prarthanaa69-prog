45810312e4406da471b6a76e526f0c038fa56b9c
prarthanaa69-prog <prarthanaa69@gmail.com>
2026-04-30 22:12:06 +0530

    Update Q05.py

--- Q05.py
+++ Q05.py
@@ -17,3 +17,15 @@
 #   FizzBuzz
 
 # --- YOUR CODE HERE ---
+n=int(input("Enter n:"))
+
+
+for i in range(1,n+1):
+    if(i%3==0 and i%5!=0):
+        print("Fizz")
+    elif(i%5==0 and i%3!=0):
+        print("Buzz")
+    elif(i%3==0 and i%5==0):
+        print("FizzBuzz")
+    else:
+        print(i)
