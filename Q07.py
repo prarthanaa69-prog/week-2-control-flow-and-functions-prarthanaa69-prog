d39cfeeb1964ae44483030ec9943b6d291458e6e
prarthanaa69-prog <prarthanaa69@gmail.com>
2026-05-01 10:39:22 +0530

    Update Q07.py

--- Q07.py
+++ Q07.py
@@ -10,3 +10,13 @@
 # Sample Output 2:  Reversed: 5
 
 # --- YOUR CODE HERE ---
+n=int(input("Enter a number: "))
+
+rev=0
+rem=0
+while(n!=0):
+    rem=int(n%10)
+    rev=rev*10+rem
+    n=int(n/10)
+
+print("Reversed: ",rev)
